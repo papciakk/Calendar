f683d08c6ccb8b791ce485a39e2270974e7bf5a0
Krzysztof Papciak <papciakk@student.agh.edu.pl>
2016-06-26 14:53:52 +0200

    dev #13

--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -1,29 +1,50 @@
+function validateLoginInputs() {
+    return true;
+}
+
+function validateRegisterInputs() {
+    return true;
+}
+
 function initUserEvents() {
     $("#btn_modal_login").click(function () {
 
+        if (!validateLoginInputs()) return;
+
         var username = $("#login_username").val();
         var password = $("#login_password").val();
+
+        $('#login_modal').modal('hide');
 
         userResource.login(
             username,
             password,
             function () {
-                $('#login_modal').modal('hide');
                 if (showConfirmAlets) {
                     bootbox.alert("Zalogowano jako " + username);
                 }
             },
             function () {
-                $('#login_modal').modal('hide');
-                if (showConfirmAlets) {
-                    bootbox.alert("Niepoprawny login lub hasło");
-                }
+                bootbox.alert("Niepoprawny login lub hasło");
             }
         );
     });
 
     $("#btn_modal_register").click(function () {
-        
+        var username = $("#register_username").val();
+        var password = $("#register_password").val();
+        var passwordr = $("#register_password_repeat").val();
+
+        $('#register_modal').modal('hide');
+
+        if (password == passwordr) {
+            userResource.register(username, password, function () {
+
+            });
+        } else {
+            bootbox.alert("Podane hasła nie sa takie same");
+        }
+
     });
 
     $("#btn_logout").click(function () {
